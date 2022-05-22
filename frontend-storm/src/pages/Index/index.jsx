3009d83e7a334feaf5c992057c9af1f5c59d6247
Guilherme <guilhermesaldanha08@gmail.com>
2022-05-22 15:08:30 -0300

    feat: ItemCarrinho

--- frontend-storm/src/pages/Index/index.jsx
+++ frontend-storm/src/pages/Index/index.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
-import Carousel from '../../components/Carousel';
-import Categoria from '../../components/Categoria';
-import Categorias from '../../components/Categorias';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Carousel from "../../components/Carousel";
+import Categorias from "../../components/Categorias";
+import ItemCarrinho from "../../components/ItemCarrinho";
 
 function Index() {
-    return (
-        <div>
-            <Navbar />
-            <div className='body'>
-                <div className='cabecalho'>
+  return (
+    <div>
+      <Navbar />
+      <div className="body">
+        <div className="cabecalho"></div>
 
-                </div>
-                
-                <div className='principal' style={{width:'100%'}}>
-                    <Carousel />
-                    <Categorias/>
-                </div>
-
-            </div>
-            <Footer />
+        <div className="principal" style={{ width: "100%" }}>
+          <Carousel />
+          <Categorias />
+          <ItemCarrinho nomeProduto="teste" preco="124,89" numParcelas="12x" />
         </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
 export default Index;
