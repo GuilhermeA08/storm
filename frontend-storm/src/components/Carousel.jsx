a5f3500ec272431cff142804735587d16e366694
Guilherme <guilhermesaldanha08@gmail.com>
2022-05-24 08:52:02 -0300

    fix: teste 2

--- frontend-storm/src/components/Carousel.jsx
+++ frontend-storm/src/components/Carousel.jsx
@@ -1,65 +1,62 @@
-import {React, useRef} from 'react';
-import ItemCatalogo from './ItemCatalogo';
-import '../assets/css/carousel.css'
+import { React, useRef } from "react";
+import ItemCatalogo from "./ItemCatalogo";
+import "../assets/css/carousel.css";
 
 function Carousel() {
-    const carousel = useRef(null);
-    
-    const handleLeftClick = (e) => {
-        e.preventDefault();
-        carousel.current.scrollLeft -= carousel.current.offsetWidth;
-    }
+  const carousel = useRef(0);
 
-    const handleRightClick = (e) => {
-        e.preventDefault();
-        carousel.current.scrollLeft += carousel.current.offsetWidth;
-    }
-    
-    return (
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
 
-        <div> 
-            <div className='carousel' ref={carousel}>
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
-            </div>
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
 
-            <div className='botoes'>
-                <button onClick={handleLeftClick}>
-                    <img src="image/angle.png" alt="scroll left" />
-                </button>
-                <button onClick={handleRightClick}>
-                    <img src="image/angle.png" alt="scroll right" />
-                </button>
-            </div>
-        </div>
-        
-        
-    );
+  return (
+    <div>
+      <div className="carousel" ref={carousel}>
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+        <ItemCatalogo imagem="" nome="Carro" preco="00,00" parcelas="0 x R$ 00,00" frete="gratis" />
+      </div>
+
+      <div className="botoes">
+        <button onClick={handleLeftClick}>
+          <img src="image/angle.png" alt="scroll left" />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src="image/angle.png" alt="scroll right" />
+        </button>
+      </div>
+    </div>
+  );
 }
 export default Carousel;
