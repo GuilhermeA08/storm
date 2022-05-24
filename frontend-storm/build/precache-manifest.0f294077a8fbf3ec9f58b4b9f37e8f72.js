b191f9e7b7133e9ff29673a48340a08bbdec8f35
Guilherme <guilhermesaldanha08@gmail.com>
2022-05-24 08:27:32 -0300

    fix: vercel

---
+++ frontend-storm/build/precache-manifest.0f294077a8fbf3ec9f58b4b9f37e8f72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6736967a10eb9a37d58",
+    "url": "/static/css/main.f447c4fb.chunk.css"
+  },
+  {
+    "revision": "a6736967a10eb9a37d58",
+    "url": "/static/js/main.a6736967.chunk.js"
+  },
+  {
+    "revision": "01e4d3ca527e17b58fbb",
+    "url": "/static/js/1.01e4d3ca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf7e2ed2b456843c763a99e32d83716a",
+    "url": "/static/media/suporte-tecnico.cf7e2ed2.png"
+  },
+  {
+    "revision": "a49f2cfad22d6b5e8b424b96f7ccfa2b",
+    "url": "/static/media/caminhao-de-entrega.a49f2cfa.png"
+  },
+  {
+    "revision": "05b2565c3321817144367bc4c45303cc",
+    "url": "/static/media/certificado-de-garantia.05b2565c.png"
+  },
+  {
+    "revision": "2e0f71728cf1f1eb9ca130c02a2af16a",
+    "url": "/static/media/kisspng-website-e-commerce-world-wide-web-digital-marketin-mobile-online-shopping-5aa34511625da9.4854262815206494894029.2e0f7172.png"
+  },
+  {
+    "revision": "f96b2b6ffec2e978cd3948e6a42a238e",
+    "url": "/static/media/sacola.png.f96b2b6f.png"
+  },
+  {
+    "revision": "5c5cac2a378ddd6c73b25b07f04b28b5",
+    "url": "/static/media/topo1.5c5cac2a.png"
+  },
+  {
+    "revision": "f6a50e4c9efa2f6c4d91a383bae400d8",
+    "url": "/static/media/Quicksand_Book.f6a50e4c.woff"
+  },
+  {
+    "revision": "e9eb8fbe5e5e966ee30b547171c2453d",
+    "url": "/static/media/Cubos.e9eb8fbe.woff"
+  },
+  {
+    "revision": "cc36885b7bbd823ab49c0e1d4fa0071f",
+    "url": "/static/media/fundo login.cc36885b.png"
+  },
+  {
+    "revision": "1dbbe04100b5685385d31c46bf552048",
+    "url": "/index.html"
+  }
+];
